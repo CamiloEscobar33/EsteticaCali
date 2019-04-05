3acfce73218b4f9aad8fa2289f2e7c58150da69c
unknown <camilo_aea@hotmail.com>
2019-04-05 12:30:35 -0500

    Codigo de estetica de cali

---
+++ src/Model/Empleado.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Empleado {
+
+	//atritbutos
+	private String nombre;
+
+	private int cedula;
+
+	private String cargo;
+
+	private int anioIngreso;
+
+	public Contenedor (String nombre, int cedula, String cargo, int anioIngreso) {
+
+		this.nombre = nombre;
+
+		this.cedula = cedula;
+
+		this.cargo = cargo;
+
+		this.cargo = anioIngreso;
+
+	}
+
+	
+}
